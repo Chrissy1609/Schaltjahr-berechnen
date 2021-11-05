15bedcd694db8c51b84725187766999099900602
Your Name <you@example.com>
2021-11-05 17:58:25 +0100

    Aufgabe 1 und 2 bearbeitet

---
+++ Schaljahr_berechnung_Aufgabe2.py
@@ -0,0 +1,14 @@
+# Name   : Christian Meier
+# Klasse : ETS 1
+# Datum  : 05.11.2021
+
+Jahre =[]                                                                           # Liste die Ausgegeben wird
+
+minJahr = 1799                                                                      # Start der Liste
+maxJahr = 2301                                                                      # Ende  der Liste
+for i in range(maxJahr - minJahr):                                                  # 
+    pruefJahr = minJahr + i                                                         # Das Jahr das überprüft wird
+    if (pruefJahr % 4 ==0 and (pruefJahr % 100 != 0 or pruefJahr % 400 ==0)):       # % bedeute den Rest der Ganzzahl ausgeben
+        Jahre.append(pruefJahr)                                                     # == bedeutet ist gleich / != bedeutet ist nicht gleich
+                                                                                    # (Jahre) = Liste / .append = etwas zur Liste hinzufügen
+print(Jahre)                                                                        # Liste Jahre ausgeben
---
+++ Schaltjahr_berechnen_Aufgabe1.py
@@ -0,0 +1,12 @@
+# Name   : Christian Meier
+# Klasse : ETS 1
+# Datum  : 05.11.2021
+
+prüfJahr = 2100
+
+if prüfJahr % 4 == 0 and (prüfJahr % 100 !=0 or prüfJahr % 400 ==0):   # %      bedeutet: gebe den Rest einer Ganzzahl aus
+    print(prüfJahr ,"ist ein Schaltjahr")                              # ==     bedeutet: ist gleich
+                                                                       # !=     bedeutet: ist nicht gleich
+else:                                                                  # else   bedeutet: wenn nicht dann
+    print(prüfJahr ,"ist kein Schaltjahr")                             # if     bedeutet:   wenn es so ist
+
