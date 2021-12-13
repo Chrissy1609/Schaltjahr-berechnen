067489e48fb83220408407bfb2b960ab0465e37e
Chrissy1609 <91952546+Chrissy1609@users.noreply.github.com>
2021-12-13 17:25:04 +0100

    Kommentiert

--- Schaljahr_berechnung_Aufgabe2.py
+++ Schaljahr_berechnung_Aufgabe2.py
@@ -6,9 +6,9 @@ Jahre =[]                                                                       
 
 minJahr = 1799                                                                      # Start der Liste
 maxJahr = 2301                                                                      # Ende  der Liste
-for i in range(maxJahr - minJahr):                                                  # 
+for i in range(maxJahr - minJahr):                                                  # Anzahl der Durchgänge der Schleife
     pruefJahr = minJahr + i                                                         # Das Jahr das überprüft wird
     if (pruefJahr % 4 ==0 and (pruefJahr % 100 != 0 or pruefJahr % 400 ==0)):       # % bedeute den Rest der Ganzzahl ausgeben
         Jahre.append(pruefJahr)                                                     # == bedeutet ist gleich / != bedeutet ist nicht gleich
                                                                                     # (Jahre) = Liste / .append = etwas zur Liste hinzufügen
-    print(Jahre)                                                                        # Liste Jahre ausgeben
+    print(Jahre)                                                                    # Liste Jahre ausgeben
--- Schaltjahr_berechnen_Aufgabe1.py
+++ Schaltjahr_berechnen_Aufgabe1.py
@@ -2,7 +2,7 @@
 # Klasse : ETS 1
 # Datum  : 05.11.2021
 
-prüfJahr = 2100
+prüfJahr = 2100                                                        # Wert einem Namen zugeordnet
 
 if prüfJahr % 4 == 0 and (prüfJahr % 100 !=0 or prüfJahr % 400 ==0):   # %      bedeutet: gebe den Rest einer Ganzzahl aus
     print(prüfJahr ,"ist ein Schaltjahr")                              # ==     bedeutet: ist gleich
